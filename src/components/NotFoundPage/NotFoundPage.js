d5bf40a318ec40da5cd9d01268271fbc4a40cb35
Elena Kainarova <helenrizik@yandex.ru>
2023-09-13 02:30:50 +0300

    complete 404page 768/320

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -3,15 +3,15 @@ import "./NotFoundPage.css";
 
 function PageNotFound() {
   return (
-    <section className="not-found">
-      <div className="not-found__container">
-        <h1 className="not-found__title">404</h1>
-        <p className="not-found__subtitle">Страница не найдена</p>
-      </div>
+    <main className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__subtitle">Страница не найдена</p>
+
       <Link to="/" className="not-found__link">
         Назад
       </Link>
-    </section>
+      
+    </main>
   );
 }
 
